caa476ddd5a1c967ba6febc589942c1587e4581a
k4ioken <146067689+k4ioken@users.noreply.github.com>
2024-07-01 20:57:51 +0000

    data type conversion introduction

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,10 @@
+let age=19
+let stringage=String(age) //converts age from number to string type
+
+let test="3abc4"
+
+let numbertest=Number(test)
+console.log(typeof numbertest); //returns type number
+console.log(numbertest); //returns NaN
+ //printing this will return NaN (not a number) as the string characters cant be converted to number
+
