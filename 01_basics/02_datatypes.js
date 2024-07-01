43a4797d9ee5b09591301467cd8fcca1f7f855b2
k4ioken <146067689+k4ioken@users.noreply.github.com>
2024-07-01 20:28:11 +0000

    datatypes introduction

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" //treats all js code as newer version
+
+let name="ABCD" //string
+let age =19  //number type
+
+//bigInt-> like long in c++
+//null --> returns empty value 
+//symbol --> used later in react
+//undefined --> like let name;
+//bool 
+//object 
+console.log(name);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null);  //returns objects datatype
+console.log(typeof undefined);  //returns undefined datatype
+
